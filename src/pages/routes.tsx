647671e88e6466b8cdd70ebf618317825ecff3c7
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-01-31 18:58:00 -0300

    Adicionando arquivo de rotas

---
+++ src/pages/routes.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { Login } from "./Login";
+import { Pagina404 } from "./Pagina404";
+import { RefeicaoCadastro } from "./Refeicao/RefeicaoCadastro";
+import { RefeicaoDados } from "./Refeicao/RefeicaoDados";
+import { RefeicaoLista } from "./Refeicao/RefeicaoLista";
+import { UsuarioCadastro } from "./Usuario/UsuarioCadastro";
+
+export function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/usuario/cadastro" element={<UsuarioCadastro/>} />
+        <Route path="/usuario/:id" element={<UsuarioCadastro/>} />
+        <Route path="/home" element={<HomePage/>} />
+        <Route path="/refeicao/cadastro" element={<RefeicaoCadastro/>} />
+        <Route path="/refeicao/:id" element={<RefeicaoDados/>} />
+        <Route path="/refeicao" element={<RefeicaoLista/>} />
+        <Route element={<Pagina404 />} path="*" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
