c26f9c2d32cdb303a9e225bc5947b8f09130e7bf
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-01 19:52:25 -0300

    Adicionando filtro da tabela

---
+++ src/components/Filtros/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useAsyncDebounce } from "react-table";
+import { Form, Label, InputGroup, InputGroupText, Input } from "reactstrap";
+
+interface CampoFiltroGlobalTabelaProps {
+  globalFilter: any;
+  setGlobalFilter: any;
+}
+
+export function CampoFiltroGlobalTabela(props: CampoFiltroGlobalTabelaProps) {
+  const [value, setValue] = useState(props.globalFilter);
+  const onChange = useAsyncDebounce(value => {
+    props.setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <Form className="d-flex flex-row align-items-center" onSubmit={event => event.preventDefault()}>
+      <Label for="filter_table" className="mb-0 me-3 fw-bold form-label">Pesquisar</Label>
+      <InputGroup>
+        <InputGroupText>
+          <FaSearch />
+        </InputGroupText>
+        <Input
+          className="rounded-pill"
+          type="text"
+          id="filter_table"
+          value={value || ""}
+          onChange={event => {
+            setValue(event.target.value);
+            onChange(event.target.value);
+          }}
+          placeholder="Busca"
+        />
+      </InputGroup>
+    </Form>
+  );
+}
