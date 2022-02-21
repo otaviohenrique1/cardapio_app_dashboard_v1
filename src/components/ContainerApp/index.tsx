2d86b86f63d4c7d731623d46829d1a701973f41d
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-21 16:32:53 -0300

    alterando logica do login

--- src/components/ContainerApp/index.tsx
+++ src/components/ContainerApp/index.tsx
@@ -1,16 +1,49 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Container } from "reactstrap";
-import { HeaderApp, UsuarioLogadoDataTypes } from "../HeaderApp";
+import { RootState } from "../../app/store";
+import { adicionaLogin } from "../../features/login/LoginSlice";
+import { dadosIniciaisUsuarioLogado, HeaderApp, UsuarioLogadoDataTypes } from "../HeaderApp";
 
 interface ContainerAppProps {
   children: ReactNode;
-  data_usuario_logado?: UsuarioLogadoDataTypes;
 }
 
 export function ContainerApp(props: ContainerAppProps) {
+  const [data, setData] = useState<UsuarioLogadoDataTypes>(dadosIniciaisUsuarioLogado);
+
+  const selector = useSelector((state: RootState) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(selector.login.id &&
+      selector.login.nome) {
+      setData({
+        id: selector.login.id,
+        nome: selector.login.nome,
+      });
+    } else {
+      let id = sessionStorage.getItem('id');
+      let nome = sessionStorage.getItem('nome');
+  
+      if (id && nome) {
+        dispatch(adicionaLogin({
+          id: id,
+          nome: nome,
+        }));
+        
+        setData({
+          id: selector.login.id,
+          nome: selector.login.nome,
+        });
+      }
+    }
+    
+  }, [dispatch, selector.login.id, selector.login.nome]);
+
   return (
     <>
-      <HeaderApp data_usuario_logado={props.data_usuario_logado}/>
+      <HeaderApp data_usuario_logado={data} />
       <Container className="mt-2">{props.children}</Container>
     </>
   );
