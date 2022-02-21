9ec70d7cff151d888ee6de46e06cea7797202236
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-21 16:33:20 -0300

    alterando logica do login

--- src/components/HeaderApp/index.tsx
+++ src/components/HeaderApp/index.tsx
@@ -1,7 +1,7 @@
 import { Nav, Navbar, NavItem, Collapse, DropdownItem, DropdownMenu, DropdownToggle, NavbarBrand, NavbarToggler, Dropdown } from "reactstrap";
 import { BiUserCircle } from "react-icons/bi";
 import { MdMenuBook } from "react-icons/md";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
@@ -9,32 +9,21 @@ import withReactContent from 'sweetalert2-react-content';
 const SwalModal = withReactContent(Swal);
 
 export interface UsuarioLogadoDataTypes {
-  id_usuario: string;
+  id: string;
   nome: string;
 }
 
 interface HeaderAppProps {
-  data_usuario_logado?: UsuarioLogadoDataTypes;
+  data_usuario_logado: UsuarioLogadoDataTypes;
 }
 
-const dadosIniciais: UsuarioLogadoDataTypes = {
-  id_usuario: '',
+export const dadosIniciaisUsuarioLogado: UsuarioLogadoDataTypes = {
+  id: '',
   nome: ''
 };
 
 export function HeaderApp(props: HeaderAppProps) {
-  const [data, setData] = useState<UsuarioLogadoDataTypes>(dadosIniciais);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (props.data_usuario_logado) {
-      setData(props.data_usuario_logado);
-    }
-    setData({
-      id_usuario: 'id_usuario',
-      nome: 'nome_usuario'
-    });
-  }, [props.data_usuario_logado]);
 
   const [aberto, setAberto] = useState<boolean>(false);
   const toggle = () => {
@@ -78,7 +67,7 @@ export function HeaderApp(props: HeaderAppProps) {
           </NavItem>
           <Dropdown toggle={toggleDropdown} isOpen={dropdownAberto}>
             <DropdownToggle caret className="d-flex flex-row justify-content-center align-items-center">
-              <h6 className="m-0">{(data.nome) ? data.nome : '[Nome do usuario]'}</h6>
+              <h6 className="m-0">{props.data_usuario_logado.nome}</h6>
               <BiUserCircle size={30} className="ms-2" />
             </DropdownToggle>
             <DropdownMenu dark>
