d488e97604c74940c0b030365d95e741d8f2b035
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-23 22:59:38 -0300

    Adicionando rotas UsuarioEdicao, Calendario e TesteUuid

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -6,8 +6,10 @@ import { Pagina404 } from "./Pagina404";
 import { RefeicaoCadastro } from "./Refeicao/RefeicaoCadastro";
 import { RefeicaoDados } from "./Refeicao/RefeicaoDados";
 import { RefeicaoEdicao } from "./Refeicao/RefeicaoEdicao";
+import { TesteUuid } from "./TesteUuid";
 import { UsuarioCadastro } from "./Usuario/UsuarioCadastro";
 import { UsuarioDados } from "./Usuario/UsuarioDados";
+import { UsuarioEdicao } from "./Usuario/UsuarioEdicao";
 
 export function RoutesApp() {
   return (
@@ -16,12 +18,14 @@ export function RoutesApp() {
         <Route path="/" element={<Login/>} />
         <Route path="/usuario/cadastro" element={<UsuarioCadastro/>} />
         <Route path="/usuario/:id" element={<UsuarioDados/>} />
+        <Route path="/usuario/:id/edicao" element={<UsuarioEdicao />} />
         <Route path="/home" element={<HomePage/>} />
         <Route path="/refeicao/cadastro" element={<RefeicaoCadastro/>} />
         <Route path="/refeicao/:id" element={<RefeicaoDados />} />
         <Route path="/refeicao/:id/edicao" element={<RefeicaoEdicao />} />
         <Route element={<Pagina404 />} path="*" />
-        <Route path="/calendario" element={<Calendario/>} />
+        <Route path="/calendario" element={<Calendario/>} /> {/* Pagina de teste, Retirar */}
+        <Route path="/testeuuid" element={<TesteUuid/>} /> {/* Pagina de teste, Retirar */}
       </Routes>
     </BrowserRouter>
   );
