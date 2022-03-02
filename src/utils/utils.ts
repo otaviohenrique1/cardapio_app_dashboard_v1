6e6fc03dc317027db701e5c2eb3efddc37f2527b
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-01 22:19:01 -0300

    adicionando comentarios de exemplo e funcões ConverteArrayObjetosParaString, ConverteStringParaArrayObjetos, ConverteArrayObjetosParaString2 e ConverteStringParaArrayObjetos2

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -21,3 +21,57 @@ export function Mensagem(texto: string) {
 export function MensagemErro(campo: string) {
   return Mensagem(`Campo ${campo} esta vazio`);
 }
+
+/*
+  let users = [{ name: 'jeca' }, { name: 'jaca' }];
+  
+  Exemplo de Array de Objetos para String
+    let result = users.map(x => x.name).join(';');
+
+  Exemplo de String para Array de Objetos
+    let result2 = result.split(';');
+    let result3 = result2.map(x => {
+      return { name: x };
+    });
+*/
+
+/**
+ * Converte array de objetos lista de ingredientes para uma string
+ */
+export function ConverteArrayObjetosParaString(lista: { nome: string }[]) {
+  let conveteParaArraySimples = lista.map(item => item.nome);
+  let converteParaString = conveteParaArraySimples.join(';');
+  return converteParaString;
+}
+
+/**
+ * Converte string lista de ingredientes para uma array de objetos
+ */
+export function ConverteStringParaArrayObjetos(texto: string) {
+  let converteParaArraySimples = texto.split(';');
+  let converteParaArrayDeObjetos = converteParaArraySimples.map(item => {
+    return { nome: item };
+  });
+  return converteParaArrayDeObjetos;
+}
+
+/*
+  Exemplo
+    let myArr = [{x1:0,x2:2000,y:300},{x1:50,x2:250,y:500}];
+    let myArrString = JSON.stringify(myArr);
+    let myArrParse = JSON.parse(myArrString);
+*/
+
+/**
+ * Converte array de objetos lista de ingredientes para uma string
+ */
+export function ConverteArrayObjetosParaString2(lista: { nome: string }[]) {
+  return JSON.stringify(lista);
+}
+
+/**
+ * Converte string lista de ingredientes para uma array de objetos
+ */
+export function ConverteStringParaArrayObjetos2(texto: string) {
+  return JSON.parse(texto);
+}
