e015303d1d82941c13b7764fde12206f2cfe1276
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-02 16:58:08 -0300

    alterandp atributo valor

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -10,7 +10,7 @@ export function ItemListaFichaDados(props: ItemListaFichaDadosProps) {
   return (
     <ListGroupItem className="d-flex flex-row justify-content-between">
       <Titulo tag="h5" className="w-100">{props.titulo}</Titulo>
-      <Titulo tag="h5" className="w-100">{props.valor}</Titulo>
+      <Titulo tag="h6" className="w-100">{props.valor}</Titulo>
     </ListGroupItem>
   );
 }
