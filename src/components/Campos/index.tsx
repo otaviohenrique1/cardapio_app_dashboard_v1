811b6e73e78779e674232f7e278a0d9b505b7c30
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-02 22:20:47 -0300

    Adicionando componente CampoCheckbox e alterando nome do campoformulario para CampoInput

--- src/components/Campos/index.tsx
+++ src/components/Campos/index.tsx
@@ -1,8 +1,9 @@
 import { Alert, Col, Label } from "reactstrap";
 import { Field } from "formik";
 import { ColumnProps } from "reactstrap/types/lib/Col";
+import { ReactNode } from "react";
 
-interface CampoFormularioCadastroProps {
+interface CampoInputProps {
   md: ColumnProps;
   label: string;
   id: string;
@@ -14,13 +15,32 @@ interface CampoFormularioCadastroProps {
   touched?: any;
 }
 
-export function CampoFormularioCadastro(props: CampoFormularioCadastroProps) {
+export function CampoInput(props: CampoInputProps) {
   return (
     <Col md={props.md} className="d-flex flex-column mt-3">
       <Label className="form-label" htmlFor={props.id}>{props.label}</Label>
       <Field  className="form-control" id={props.id} name={props.name}
         type={props.type} value={props.value} placeholder={props.placeholder} />
       {props.error && props.touched ? (<Alert color="danger">{props.error}</Alert>) : null}
+    </Col>
+  );
+}
+
+interface CampoCheckboxProps {
+  name: string;
+  children: ReactNode;
+  checked?: boolean;
+}
+
+export function CampoCheckbox(props: CampoCheckboxProps) {
+  return (
+    <Col md={12} className="d-flex flex-row">
+      <Field
+        className="form-check"
+        type="checkbox"
+        name={props.name}
+      />
+      <Label className="form-label ms-2">{props.children}</Label>
     </Col>
   );
 }
