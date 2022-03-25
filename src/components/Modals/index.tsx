5d7b1629cd8df28b88b86c903e596cb196f0bce4
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-25 18:48:45 -0300

    refatorando mensagens de erro e sucesso

---
+++ src/components/Modals/index.tsx
@@ -0,0 +1,28 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const SwalModal = withReactContent(Swal);
+
+export function ModalConfirmacaoCadastro() {
+  return SwalModal.fire({
+    icon: 'success',
+    title: "Cadastro realizado com sucesso!",
+    buttonsStyling: false,
+    confirmButtonText: 'Fechar',
+    customClass: {
+      confirmButton: 'btn btn-primary',
+    },
+  });
+}
+
+export function ModalErroCadastro(erro: any) {
+  return SwalModal.fire({
+    icon: 'error',
+    title: "Erro!",
+    html: <p>{erro}</p>,
+    buttonsStyling: false,
+    confirmButtonText: 'Fechar',
+    customClass: {
+      confirmButton: 'btn btn-primary',
+    },
+  });
+}
