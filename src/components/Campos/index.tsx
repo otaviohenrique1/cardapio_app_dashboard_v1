66e45b4a042b8017bfc8510c4204775720a190cb
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-25 18:44:03 -0300

    adicionando componente CampoTextArea

--- src/components/Campos/index.tsx
+++ src/components/Campos/index.tsx
@@ -19,8 +19,30 @@ export function CampoInput(props: CampoInputProps) {
   return (
     <Col md={props.md} className="d-flex flex-column mt-3">
       <Label className="form-label" htmlFor={props.id}>{props.label}</Label>
-      <Field  className="form-control" id={props.id} name={props.name}
+      <Field className="form-control" id={props.id} name={props.name}
         type={props.type} value={props.value} placeholder={props.placeholder} />
+      {props.error && props.touched ? (<Alert color="danger">{props.error}</Alert>) : null}
+    </Col>
+  );
+}
+
+interface CampoTextAreaProps {
+  md: ColumnProps;
+  label: string;
+  id: string;
+  name: string;
+  value: string;
+  placeholder: string;
+  error?: any;
+  touched?: any;
+}
+
+export function CampoTextArea(props: CampoTextAreaProps) {
+  return (
+    <Col md={props.md} className="d-flex flex-column mt-3">
+      <Label className="form-label" htmlFor={props.id}>{props.label}</Label>
+      <Field id={props.id} name={props.name} as="textarea"
+        className="form-control" placeholder={props.placeholder} value={props.value} />
       {props.error && props.touched ? (<Alert color="danger">{props.error}</Alert>) : null}
     </Col>
   );
