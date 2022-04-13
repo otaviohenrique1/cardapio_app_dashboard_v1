8a8ffddb910d9284d315409b0a71dc36f9bd99b7
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 20:25:55 -0300

    removendo redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
