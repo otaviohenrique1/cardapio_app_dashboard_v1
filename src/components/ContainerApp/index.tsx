7e083675e136f385f9aef93feb3cf03f83eb1c31
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:26:20 -0300

    reformulando arquivo

--- src/components/ContainerApp/index.tsx
+++ src/components/ContainerApp/index.tsx
@@ -1,45 +1,29 @@
 import { ReactNode, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Container } from "reactstrap";
-import { RootState } from "../../app/store";
-import { adicionaLogin } from "../../features/login/LoginSlice";
-import { dadosIniciaisUsuarioLogado, HeaderApp, UsuarioLogadoDataTypes } from "../HeaderApp";
+import { HeaderApp } from "../HeaderApp";
+import { dadosIniciaisUsuarioLogado } from "../../utils/constantes";
 
 interface ContainerAppProps {
   children: ReactNode;
 }
 
 export function ContainerApp(props: ContainerAppProps) {
-  const [data, setData] = useState<UsuarioLogadoDataTypes>(dadosIniciaisUsuarioLogado);
-
-  const selector = useSelector((state: RootState) => state);
-  const dispatch = useDispatch();
+  const [data, setData] = useState<UsuarioLogadoTypes>(dadosIniciaisUsuarioLogado);
 
   useEffect(() => {
-    if(selector.login.id &&
-      selector.login.nome) {
-      setData({
-        id: selector.login.id,
-        nome: selector.login.nome,
-      });
-    } else {
-      let id = sessionStorage.getItem('id');
-      let nome = sessionStorage.getItem('nome');
-  
-      if (id && nome) {
-        dispatch(adicionaLogin({
-          id: id,
-          nome: nome,
-        }));
-        
-        setData({
-          id: selector.login.id,
-          nome: selector.login.nome,
-        });
-      }
-    }
-    
-  }, [dispatch, selector.login.id, selector.login.nome]);
+    let id = sessionStorage.getItem('id');
+    let nome = sessionStorage.getItem('nome');
+
+    let valida_id = (id) ? id : 'id';
+    let valida_nome = (nome) ? nome : 'nome';
+
+    const data_usuario = {
+      id: valida_id,
+      nome: valida_nome
+    };
+
+    setData(data_usuario);
+  }, []);
 
   return (
     <>
