958173812b34b344e18f760c1dc4b41a5a6efe5e
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-13 21:30:42 -0300

    reformulando arquivo

--- src/pages/Refeicao/RefeicaoDados/index.tsx
+++ src/pages/Refeicao/RefeicaoDados/index.tsx
@@ -2,41 +2,18 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Col, ListGroup, ListGroupItem, Row } from "reactstrap";
 // import { UncontrolledCarousel } from "reactstrap";
+// import { GrImage } from "react-icons/gr";
+// import { MdOutlineImageNotSupported } from "react-icons/md";
 import { ContainerApp } from "../../../components/ContainerApp";
 import { Titulo } from "../../../components/Titulo";
+import { ItemListaFichaDados } from "../../../components/Listas/ItemListaFichaDados";
+import { BotaoLink } from "../../../components/Botoes/BotaoLink";
+import { ListaIngredientes } from "../../../components/Listas/ListaIngredientes";
 import api from "../../../utils/api";
-import { FormataData, FormataValorMonetarioTexto } from "../../../utils/utils";
-import { ItemListaFichaDados } from "../../../components/Lista";
-import { MdPlayArrow } from "react-icons/md";
-import { BotaoLink } from "../../../components/Botoes";
-import { GrImage } from "react-icons/gr";
-import { MdOutlineImageNotSupported } from "react-icons/md";
-
-interface RefeicaoDadosTypes {
-  id: string;
-  nome: string;
-  preco: number;
-  ativo: boolean;
-  ingredientes: { nome: string }[];
-  descricao: string;
-  codigo: string;
-  imagem: File[],
-  data_cadastro: string;
-  data_modificacao_cadastro: string;
-}
-
-const valoresIniciaisRefeicaoDados: RefeicaoDadosTypes = {
-  id: "",
-  nome: "",
-  preco: 0,
-  ativo: false,
-  ingredientes: [],
-  descricao: "",
-  codigo: "",
-  imagem: [],
-  data_cadastro: "",
-  data_modificacao_cadastro: "",
-};
+import { ModalErroCadastro } from "../../../components/Modals";
+import { FormatadorDados } from "../../../utils/FormatadorDados";
+import { ConversorListas } from "../../../utils/ConversorListas";
+import { valoresIniciaisRefeicaoDados } from "../../../utils/constantes";
 
 export function RefeicaoDados() {
   const [data, setData] = useState<RefeicaoDadosTypes>(valoresIniciaisRefeicaoDados);
@@ -45,26 +22,34 @@ export function RefeicaoDados() {
   useEffect(() => {
     api.get(`refeicao/${id}`)
       .then((item) => {
-        if (!id) {
-          return;
-        }
+        if (!id) { return; }
 
-        const nome = item.data.nome;
-        const preco = item.data.preco;
-        const ativo = item.data.ativo;
-        const ingredientes = JSON.parse(String(item.data.ingredientes));
-        const descricao = item.data.descricao;
-        const codigo = item.data.codigo;
-        const imagem = [...item.data.imagem];
-        const data_cadastro = FormataData(item.data.data_cadastro);
-        const data_modificacao_cadastro = FormataData(item.data.data_cadastro);
+        const { nome, codigo, preco, ingredientes, descricao, ativo, data_cadastro, data_modificacao_cadastro, imagem } = item.data;
 
-        const data = { id, nome, preco, ativo, ingredientes, descricao, codigo, imagem, data_cadastro, data_modificacao_cadastro };
+        const preco_formatado = FormatadorDados.FormataValorMonetarioTexto(preco);
+        const status_refeicao = FormatadorDados.ValidaStatusRefeicao(ativo);
+        const ingredientes_lista = ConversorListas.ConverteStringParaArrayObjetos(ingredientes);
+        const data_cadastro_formatada = FormatadorDados.FormatadorDataHora(data_cadastro, "dd/MM/yyyy HH:mm");
+        const data_modificacao_cadastro_formatada = FormatadorDados.FormatadorDataHora(data_modificacao_cadastro, "dd/MM/yyyy HH:mm");
+
+        const data = {
+          id,
+          nome: String(nome),
+          preco: preco_formatado,
+          ativo: status_refeicao,
+          ingredientes: ingredientes_lista,
+          descricao: String(descricao),
+          codigo: String(codigo),
+          imagens: [...imagem],
+          data_cadastro: data_cadastro_formatada,
+          data_modificacao_cadastro: data_modificacao_cadastro_formatada
+        };
 
         setData(data);
       })
-      .catch((erro) => {
-        console.error(erro);
+      .catch((error) => {
+        ModalErroCadastro();
+        console.error(error);
       });
   }, [id]);
 
@@ -76,51 +61,22 @@ export function RefeicaoDados() {
         </Col>
         <Col md={12}>
           <ListGroup>
-            <ListGroupItem className="d-flex justify-content-center">
+            {/* <ListGroupItem className="d-flex justify-content-center">
               {(data.imagem.length === 0) ? (
                 <MdOutlineImageNotSupported size={100} />
               ) : (
                 <GrImage size={100} />
               )}
-            </ListGroupItem>
-            <ItemListaFichaDados
-              titulo="Código"
-              valor={data.id}
-            />
-            <ItemListaFichaDados
-              titulo="Preço (R$)"
-              valor={FormataValorMonetarioTexto(data.preco)}
-            />
-            <ItemListaFichaDados
-              titulo="Status"
-              valor={(data.ativo) ? 'Ativo' : 'Inativo'}
-            />
-            <ItemListaFichaDados
-              titulo="Descrição"
-              valor={data.descricao}
-            />
-            <ItemListaFichaDados
-              titulo="Código"
-              valor={data.codigo}
-            />
-            <ItemListaFichaDados
-              titulo="Data de cadastro"
-              valor={data.data_cadastro}
-            />
-            <ItemListaFichaDados
-              titulo="Data de atualização do cadastro"
-              valor={data.data_modificacao_cadastro}
-            />
-            <ListGroupItem className="d-flex flex-column">
-              <Titulo tag="h5" className="w-100">Ingredientes</Titulo>
-              {data.ingredientes.map((item, index) => {
-                return (
-                  <div className="d-flex flex-row justify-content-between" key={index}>
-                    <MdPlayArrow size={25} className="me-1" />
-                    <Titulo tag="h6" className="w-100">{item.nome}</Titulo>
-                  </div>
-                );
-              })}
+            </ListGroupItem> */}
+            <ItemListaFichaDados titulo="Código" valor={data.id} />
+            <ItemListaFichaDados titulo="Preço (R$)" valor={data.preco} />
+            <ItemListaFichaDados titulo="Status" valor={data.ativo} />
+            <ItemListaFichaDados titulo="Descrição" valor={data.descricao} />
+            <ItemListaFichaDados titulo="Código" valor={data.codigo} />
+            <ItemListaFichaDados titulo="Data de cadastro" valor={data.data_cadastro} />
+            <ItemListaFichaDados titulo="Data de atualização do cadastro" valor={data.data_modificacao_cadastro} />
+            <ListGroupItem>
+              <ListaIngredientes data={data.ingredientes} />
             </ListGroupItem>
           </ListGroup>
         </Col>
