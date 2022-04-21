003d13c9d4248e4da63a69b452658ee944d22321
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-21 18:53:34 -0300

    organizando imports alterando codigo dentro do useEffect alterando a lista de dados

--- src/pages/Refeicao/RefeicaoDados/index.tsx
+++ src/pages/Refeicao/RefeicaoDados/index.tsx
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Col, ListGroup, ListGroupItem, Row } from "reactstrap";
-// import { UncontrolledCarousel } from "reactstrap";
-// import { GrImage } from "react-icons/gr";
-import { MdOutlineImageNotSupported } from "react-icons/md";
+import { Col, ListGroup, Row } from "reactstrap";
 import { ContainerApp } from "../../../components/ContainerApp";
 import { Titulo } from "../../../components/Titulo";
-import { ItemListaFichaDados } from "../../../components/Listas/ItemListaFichaDados";
+import { ItemFichaDados } from "../../../components/Listas/ListaFichaDados/ItemFichaDados";
 import { BotaoLink } from "../../../components/Botoes/BotaoLink";
+import { ModalErroCadastro } from "../../../components/Modals";
 import api from "../../../utils/api";
-// import { ModalErroCadastro } from "../../../components/Modals";
 import { FormatadorDados } from "../../../utils/FormatadorDados";
-// import { ConversorListas } from "../../../utils/ConversorListas";
 import { FORMATO_DATA_COM_HORA_4, valoresIniciaisRefeicaoDados } from "../../../utils/constantes";
-import { IngredientesLista } from "../../../components/Listas/Ingredientes/IngredientesLista";
-import { CarouselImagens } from "../../../components/CarouselImagens";
+import { ItemFichaDadosIngredientes } from "../../../components/Listas/ListaFichaDados/ItemFichaDadosIngredientes";
+import { ItemFichaDadosImagem } from "../../../components/Listas/ListaFichaDados/ItemFichaDadosImagem";
 
 export function RefeicaoDados() {
   const [data, setData] = useState<RefeicaoDadosTypes>(valoresIniciaisRefeicaoDados);
@@ -31,19 +27,12 @@ export function RefeicaoDados() {
         const preco_formatado = FormatadorDados.FormataValorMonetarioTexto(preco);
         const status_refeicao = FormatadorDados.ValidaStatusRefeicao(ativo);
 
-        // let a = imagens as FotoTypes[];
-        // console.log("----------------------------");
-        // console.log("imagem");
-        // console.log(a);
-        // console.log("----------------------------");
-
         const data_cadastro_formatada = FormatadorDados
           .FormatadorDataHora(data_cadastro, FORMATO_DATA_COM_HORA_4);
         const data_modificacao_cadastro_formatada = FormatadorDados
           .FormatadorDataHora(data_modificacao_cadastro, FORMATO_DATA_COM_HORA_4);
 
         const ingredientes_lista_formatada = [...ingredientes] as IngredientesTypes[];
-        // const imagens_lista = [...imagem] as File[];
         const imagens_lista = [...imagens] as FotoTypes[];
 
         const data = {
@@ -51,7 +40,6 @@ export function RefeicaoDados() {
           nome: String(nome),
           preco: preco_formatado,
           ativo: status_refeicao,
-          // ingredientes: ingredientes_lista_formatada,
           ingredientes: ingredientes_lista_formatada,
           descricao: String(descricao),
           codigo: String(codigo),
@@ -63,7 +51,7 @@ export function RefeicaoDados() {
         setData(data);
       })
       .catch((error) => {
-        // ModalErroCadastro();
+        ModalErroCadastro();
         console.error(error);
       });
   }, [id]);
@@ -78,41 +66,15 @@ export function RefeicaoDados() {
         </Col>
         <Col md={12}>
           <ListGroup>
-            <ListGroupItem className="d-flex justify-content-center">
-              {/* <MdOutlineImageNotSupported size={100} /> */}
-              {(imagens.length === 0) ? (
-                <MdOutlineImageNotSupported size={100} />
-              ) : (
-                <CarouselImagens data={imagens} />
-                // <div>
-                //   {imagens.map((item, index) => {
-                //     const { id, url } = item;
-                //     return (
-                //       <img
-                //         style={{ width: '50px' }}
-                //         src={url}
-                //         alt={`imagem-${id}`}
-                //         key={index}
-                //       />
-                //       // <ul key={index}>
-                //       //   <li>{`id=>${id}`}</li>
-                //       //   <li>{`url=>${url}`}</li>
-                //       // </ul>
-                //     );
-                //   })}
-                // </div>
-              )}
-            </ListGroupItem>
-            <ItemListaFichaDados titulo="Id" valor={id} />
-            <ItemListaFichaDados titulo="Preço (R$)" valor={preco} />
-            <ItemListaFichaDados titulo="Status" valor={ativo} />
-            <ItemListaFichaDados titulo="Descrição" valor={descricao} />
-            <ItemListaFichaDados titulo="Código" valor={codigo} />
-            <ItemListaFichaDados titulo="Data de cadastro" valor={data_cadastro} />
-            <ItemListaFichaDados titulo="Data de atualização do cadastro" valor={data_modificacao_cadastro} />
-            <ListGroupItem>
-              <IngredientesLista data={ingredientes} />
-            </ListGroupItem>
+            <ItemFichaDados titulo="Id" valor={id || 'id'} />
+            <ItemFichaDados titulo="Preço (R$)" valor={preco} />
+            <ItemFichaDados titulo="Status" valor={ativo} />
+            <ItemFichaDados titulo="Descrição" valor={descricao} />
+            <ItemFichaDados titulo="Código" valor={codigo} />
+            <ItemFichaDados titulo="Data de cadastro" valor={data_cadastro} />
+            <ItemFichaDados titulo="Data de atualização do cadastro" valor={data_modificacao_cadastro} />
+            <ItemFichaDadosIngredientes data={ingredientes} />
+            <ItemFichaDadosImagem data={imagens} />
           </ListGroup>
         </Col>
         <Col md={12} className="d-flex justify-content-end mt-5">
