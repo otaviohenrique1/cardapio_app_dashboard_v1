5cb6996700ed59448bf83020a781ce2ff8ac58f2
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-26 09:43:13 -0300

    Reformulando arquivo

--- src/components/Campos/CampoListaFotos/index.tsx
+++ src/components/Campos/CampoListaFotos/index.tsx
@@ -1,11 +1,8 @@
 import { FieldArray } from "formik";
-import { AiOutlineClose } from "react-icons/ai";
-import { Card, CardBody, CardHeader, CardImg, CardText, Col, Row } from "reactstrap";
+import { Card, CardBody, CardHeader, CardImg, CardText, Col } from "reactstrap";
 import styled from "styled-components";
 import { Botao } from "../../Botoes/Botao";
 import { ModalImagem } from "../../Modals";
-import { ModalConfirmacao } from "../../Modals";
-
 interface CampoListaFotosProps {
   imagens_antigas: FotoTypes[];
 }
@@ -14,57 +11,50 @@ export function CampoListaFotos(props: CampoListaFotosProps) {
   const { imagens_antigas } = props;
 
   return (
-    <Col md={12} className="d-flex justify-content-center pb-3 mb-3 border-dark border-bottom">
+    <Col md={12} className=" d-flex flex-row justify-content-center pb-3 mb-3 border-dark border-bottom">
       <FieldArray name="imagens_antigas">
         {({ remove }) => (
-          <Row>
+          <>
             {imagens_antigas.length > 0 &&
               imagens_antigas.map((imagem_antiga, index) => {
-                const { id, url } = imagem_antiga;
+                const { id, url, nome } = imagem_antiga;
 
                 return (
-                  <Col md={3} key={index} className="p-2">
-                    <CardEstilizado>
-                      <CardHeader className="d-flex flex-row justify-content-between">
-                        <CardText className="fw-bold mb-0">ID:</CardText>
-                        <CardText className="mb-0">{id}</CardText>
-                      </CardHeader>
-                      <CardBody className="d-flex justify-content-center">
-                        <CardImgEstilizado
-                          src={url}
-                          alt={`Imagem-${id}-${index}`}
-                          onClick={() => {
-                            ModalImagem({
-                              image_url: url,
-                              image_alt: `Imagem-${id}-${index}`,
-                              image_height: '300px'
-                            });
-                          }}
-                        />
-                      </CardBody>
-                      <CardBotaoRemoverEstilizado
-                        type="button"
-                        color="danger"
-                        className="d-flex justify-content-center align-items-center w-100"
+                  <CardEstilizado key={index}
+                    className={`${(imagens_antigas.length === 2 && index === 1)
+                        ? "ms-2"
+                        : (imagens_antigas.length === 3 && index === 1)
+                          ? "ms-2 me-2"
+                          : "ms-0 me-0"
+                      }`}
+                  >
+                    <CardHeader className="d-flex flex-row justify-content-between">
+                      <CardText className="fw-bold mb-0">ID:</CardText>
+                      <CardText className="mb-0">{id}</CardText>
+                    </CardHeader>
+                    <CardBody className="d-flex justify-content-center">
+                      <CardImgEstilizado
+                        src={`${url}${nome}`}
+                        alt={`Imagem-${id}-${index}`}
                         onClick={() => {
-                          ModalConfirmacao("warning", "Aviso", "Deseja remover imagem?")
-                            .then(({ isConfirmed }) => {
-                              if (isConfirmed) {
-                                remove(index);
-                              }
-                            })
-                            .catch((error) => {
-                              console.error(error);
-                            });
+                          ModalImagem({
+                            image_url: `${url}${nome}`,
+                            image_alt: `Imagem-${id}-${index}`,
+                            image_height: '300px'
+                          });
                         }}
-                      >
-                        Remover <AiOutlineClose size={20} />
-                      </CardBotaoRemoverEstilizado>
-                    </CardEstilizado>
-                  </Col>
+                      />
+                    </CardBody>
+                    <CardBotaoRemoverEstilizado
+                      type="button"
+                      color="danger"
+                      className="d-flex justify-content-center align-items-center w-100"
+                      onClick={() => remove(index)}
+                    >Remover</CardBotaoRemoverEstilizado>
+                  </CardEstilizado>
                 );
               })}
-          </Row>
+          </>
         )}
       </FieldArray>
     </Col>
@@ -76,11 +66,12 @@ const CardBotaoRemoverEstilizado = styled(Botao)`
   border-Top-left-radius: 0 !important;
 `;
 
-const CardEstilizado = styled(Card)`
-  width: 150px !important;
-`;
-
 const CardImgEstilizado = styled(CardImg)`
   height: 100px !important;
   width: 100px !important;
+`;
+
+const CardEstilizado = styled(Card)`
+  width: 150px !important;
+
 `;
