0a03a4c533e1194a3247a7f06ece8c8276c641df
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-05-03 18:27:15 -0300

    Reformulando arquivo

--- src/components/Campos/CampoIngredientes/index.tsx
+++ src/components/Campos/CampoIngredientes/index.tsx
@@ -1,8 +1,12 @@
-import { ErrorMessage, Field, FieldArray } from "formik";
+import { ErrorMessage, FieldArray } from "formik";
 import { Card, CardBody, Col, Row } from "reactstrap";
 import styled from "styled-components";
 import { Botao } from "../../Botoes/Botao";
 import { Titulo } from "../../Titulo";
+import { CampoCheckbox } from "../CampoCheckbox";
+import { lista_unidade_quantidade } from "../../../utils/listas";
+import { CampoListaItem } from "../CampoListaItem";
+import { CampoSelect, CampoSelectProps } from "../CampoSelect";
 
 interface CampoIngredientesProps {
   ingredientes: IngredientesTypes[];
@@ -31,178 +35,50 @@ export function CampoIngredientes(props: CampoIngredientesProps) {
                 </Botao>
               </Col>
               {(ingredientes.length > 0) ? (
-                  ingredientes.map((ingrediente, index) => {
-                    const { nome, quantidade } = ingrediente;
-
-                    const ingredientes_nome = `ingredientes.${index}.nome`;
-                    const ingredientes_quantidade = `ingredientes.${index}.quantidade`;
-
-                    const campo_ingrediente_dados: CampoIngredienteProps[] = [
-                      {
-                        name_field: ingredientes_nome,
-                        placeholder_field: "Nome",
-                        type_field: "text",
-                        value_field: nome,
-                        name_error_message: ingredientes_nome
-                      },
-                      {
-                        name_field: ingredientes_quantidade,
-                        placeholder_field: "Quantidade",
-                        type_field: "number",
-                        value_field: String(quantidade),
-                        name_error_message: ingredientes_quantidade,
-                      }
-                    ];
-
-                    return (
-                      <Col md={3} key={index} className="p-2">
-                        <Card>
-                          <CardBody className="p-1">
-                            {campo_ingrediente_dados.map((item, index) => {
-                              const { name_field, placeholder_field, type_field, value_field, name_error_message } = item;
-                              return (
-                                <CampoIngrediente
-                                  key={index}
-                                  name_field={name_field}
-                                  placeholder_field={placeholder_field}
-                                  type_field={type_field}
-                                  value_field={value_field}
-                                  name_error_message={name_error_message}
-                                />
-                              );
-                            })}
-                          </CardBody>
-                          <CardBotaoRemoverEstilizado
-                            type="button"
-                            color="danger"
-                            className="d-flex justify-content-center align-items-center w-100"
-                            onClick={() => remove(index)}
-                          >Remover</CardBotaoRemoverEstilizado>
-                        </Card>
-                      </Col>
-                    );
-                  })
-                ) : (
-                  <Col md={12}>
-                    <Titulo tag="h3">Lista vazia</Titulo>
-                  </Col>
-                )}
-            </Row>
-          );
-        }}
-      </FieldArray>
-    </Col>
-  );
-}
-
-const CardBotaoRemoverEstilizado = styled(Botao)`
-  border-Top-right-radius: 0 !important;
-  border-Top-left-radius: 0 !important;
-`;
-
-interface CampoIngredienteProps {
-  name_field: string;
-  placeholder_field: string;
-  type_field: CampoInputTypes;
-  value_field: string;
-  name_error_message: string;
-}
-
-function CampoIngrediente(props: CampoIngredienteProps) {
-  const { name_field, placeholder_field, type_field, value_field, name_error_message } = props;
-
-  return (
-    <div className="d-flex flex-column mb-1">
-      <Field
-        name={name_field}
-        placeholder={placeholder_field}
-        type={type_field}
-        value={value_field}
-        className="form-control"
-      />
-      <ErrorMessage
-        name={name_error_message}
-        component="span"
-        className="text-danger"
-      />
-    </div>
-  );
-}
-
-/*
-import { ErrorMessage, Field, FieldArray } from "formik";
-import { Card, CardBody, Col, Row } from "reactstrap";
-import styled from "styled-components";
-import { Botao } from "../../Botoes/Botao";
-import { Titulo } from "../../Titulo";
-
-interface CampoIngredientesProps {
-  ingredientes: IngredientesTypes[];
-}
-
-export function CampoIngredientes(props: CampoIngredientesProps) {
-  const { ingredientes } = props;
-
-  return (
-    <Col md={12} className="d-flex flex-column pt-3 pb-3 mt-3 mb-3 border-dark border-top border-bottom">
-      <FieldArray name="ingredientes">
-        {(field_array) => {
-          const { remove, push } = field_array;
-
-          return (
-            <Row>
-              <Col md={12} className="d-flex flex-row justify-content-between pb-1">
-                <Titulo tag="h6" className="fw-normal">Lista de ingredientes</Titulo>
-                <Botao
-                  type="button"
-                  color="info"
-                  onClick={() => push({ nome: '' })}
-                  className="d-flex flex-row justify-content-center align-items-center"
-                >
-                  <span className="me-2">Adicionar ingrediente</span>
-                </Botao>
-              </Col>
-              {ingredientes.length > 0 &&
                 ingredientes.map((ingrediente, index) => {
-                  const { nome, quantidade } = ingrediente;
-
-                  const ingredientes_nome = `ingredientes.${index}.nome`;
-                  const ingredientes_quantidade = `ingredientes.${index}.quantidade`;
-
-                  const campo_ingrediente_dados: CampoIngredienteProps[] = [
-                    {
-                      name_field: ingredientes_nome,
-                      placeholder_field: "Nome",
-                      type_field: "text",
-                      value_field: nome,
-                      name_error_message: ingredientes_nome
-                    },
-                    {
-                      name_field: ingredientes_quantidade,
-                      placeholder_field: "Quantidade",
-                      type_field: "number",
-                      value_field: String(quantidade),
-                      name_error_message: ingredientes_quantidade,
-                    }
-                  ];
-
+                  const { nome, quantidade, unidade_quantidade, removivel } = ingrediente;
                   return (
                     <Col md={3} key={index} className="p-2">
                       <Card>
                         <CardBody className="p-1">
-                          {campo_ingrediente_dados.map((item, index) => {
-                            const { name_field, placeholder_field, type_field, value_field, name_error_message } = item;
-                            return (
-                              <CampoIngrediente
-                                key={index}
-                                name_field={name_field}
-                                placeholder_field={placeholder_field}
-                                type_field={type_field}
-                                value_field={value_field}
-                                name_error_message={name_error_message}
+                          <Row className="m-0 p-0">
+                            <Col md={12}>
+                              <CampoListaItem
+                                name={`ingredientes.${index}.nome`}
+                                placeholder={"Nome"}
+                                type={"text"}
+                                value={nome}
+                                name_messagem_erro={`ingredientes.${index}.nome`}
                               />
-                            );
-                          })}
+                            </Col>
+                            <Col md={6}>
+                              <CampoListaItem
+                                name={`ingredientes.${index}.quantidade`}
+                                placeholder={"Quantidade"}
+                                type={"number"}
+                                value={String(quantidade)}
+                                name_messagem_erro={`ingredientes.${index}.quantidade`}
+                              />
+                            </Col>
+                            <Col md={6}>
+                              <CampoSelectQuantidadeUnidade
+                                id={`ingredientes.${index}.quantidade`}
+                                name={`ingredientes.${index}.quantidade`}
+                                value={unidade_quantidade}
+                                placeholder="quantidade"
+                                data={lista_unidade_quantidade}
+                                name_messagem_erro={`ingredientes.${index}.quantidade`}
+                                label_item_vazio="Selecione"
+                              />
+                            </Col>
+                            <Col md={12}>
+                              <CampoCheckbox
+                                name={`ingredientes.${index}.removivel`}
+                                checked={(removivel) ? true : false}
+                                label="Removivel"
+                              />
+                            </Col>
+                          </Row>
                         </CardBody>
                         <CardBotaoRemoverEstilizado
                           type="button"
@@ -213,7 +89,12 @@ export function CampoIngredientes(props: CampoIngredientesProps) {
                       </Card>
                     </Col>
                   );
-                })}
+                })
+              ) : (
+                <Col md={12}>
+                  <Titulo tag="h3">Lista vazia</Titulo>
+                </Col>
+              )}
             </Row>
           );
         }}
@@ -227,127 +108,28 @@ const CardBotaoRemoverEstilizado = styled(Botao)`
   border-Top-left-radius: 0 !important;
 `;
 
-interface CampoIngredienteProps {
-  name_field: string;
-  placeholder_field: string;
-  type_field: CampoInputTypes;
-  value_field: string;
-  name_error_message: string;
+interface CampoSelectQuantidadeUnidadeProps extends CampoSelectProps {
+  name_messagem_erro: string;
 }
 
-function CampoIngrediente(props: CampoIngredienteProps) {
-  const { name_field, placeholder_field, type_field, value_field, name_error_message } = props;
+function CampoSelectQuantidadeUnidade(props: CampoSelectQuantidadeUnidadeProps) {
+  const { id, name, value, placeholder, data, label_item_vazio, name_messagem_erro } = props;
 
   return (
     <div className="d-flex flex-column mb-1">
-      <Field
-        name={name_field}
-        placeholder={placeholder_field}
-        type={type_field}
-        value={value_field}
-        className="form-control"
+      <CampoSelect
+        id={id}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        data={data}
+        label_item_vazio={label_item_vazio}
       />
       <ErrorMessage
-        name={name_error_message}
+        name={name_messagem_erro}
         component="span"
         className="text-danger"
       />
     </div>
   );
 }
-*/
-
-/*
-import { ErrorMessage, Field, FieldArray } from "formik";
-import { AiOutlineClose } from "react-icons/ai";
-import { GrAddCircle } from "react-icons/gr";
-import { Card, CardBody, Col, Row } from "reactstrap";
-import styled from "styled-components";
-import { Botao } from "../../Botoes/Botao";
-import { Titulo } from "../../Titulo";
-
-interface CampoIngredientesProps {
-  ingredientes: IngredientesTypes[];
-}
-
-export function CampoIngredientes(props: CampoIngredientesProps) {
-  const { ingredientes } = props;
-
-  return (
-    <Col md={12} className="d-flex flex-column pt-3 pb-3 mt-3 mb-3 border-dark border-top border-bottom">
-      <FieldArray name="ingredientes">
-        {({ remove, push }) => (
-          <Row>
-            <Col md={12} className="d-flex flex-row justify-content-between pb-1">
-              <Titulo tag="h6" className="fw-normal">Lista de ingredientes</Titulo>
-              <Botao
-                type="button"
-                color="info"
-                onClick={() => push({ nome: '' })}
-                className="d-flex flex-row justify-content-center align-items-center"
-              >
-                <span className="me-2">Adicionar ingrediente</span>
-                <GrAddCircle size={25} className="m-0 p-0" />
-              </Botao>
-            </Col>
-            {ingredientes.length > 0 &&
-              ingredientes.map((ingrediente, index) => {
-                const { nome, quantidade } = ingrediente;
-
-                return (
-                  <Col md={3} key={index} className="p-2">
-                    <Card>
-                      <CardBody className="p-1">
-                        <div className="d-flex flex-column mb-1">
-                          <Field
-                            name={`ingredientes.${index}.nome`}
-                            placeholder="Nome"
-                            type="text"
-                            value={nome}
-                            className="form-control"
-                          />
-                          <ErrorMessage
-                            name={`ingredientes.${index}.nome`}
-                            component="span"
-                            className="text-danger"
-                          />
-                        </div>
-                        <div className="d-flex flex-column">
-                          <Field
-                            name={`ingredientes.${index}.quantidade`}
-                            placeholder="Quantidade"
-                            type="number"
-                            value={quantidade}
-                            className="form-control"
-                          />
-                          <ErrorMessage
-                            name={`ingredientes.${index}.quantidade`}
-                            component="span"
-                            className="text-danger"
-                          />
-                        </div>
-                      </CardBody>
-                      <CardBotaoRemoverEstilizado
-                        type="button"
-                        color="danger"
-                        className="d-flex justify-content-center align-items-center w-100"
-                        onClick={() => remove(index)}
-                      >
-                        <AiOutlineClose size={20} />
-                      </CardBotaoRemoverEstilizado>
-                    </Card>
-                  </Col>
-                );
-              })}
-          </Row>
-        )}
-      </FieldArray>
-    </Col>
-  );
-}
-
-const CardBotaoRemoverEstilizado = styled(Botao)`
-  border-Top-right-radius: 0 !important;
-  border-Top-left-radius: 0 !important;
-`;
-*/
