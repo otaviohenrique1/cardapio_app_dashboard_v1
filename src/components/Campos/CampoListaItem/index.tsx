d34a3a314ba5f94114382ba8e4d441e896cdf66f
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-05-04 15:41:01 -0300

    adicionando prop id adicionando componente CampoInput

--- src/components/Campos/CampoListaItem/index.tsx
+++ src/components/Campos/CampoListaItem/index.tsx
@@ -1,25 +1,21 @@
-import { ErrorMessage, Field } from "formik";
-import { InputType } from "reactstrap/types/lib/Input";
+import { ErrorMessage } from "formik";
+import { CampoInput, CampoInputProps } from "../CampoInput";
 
-export interface CampoListaItemProps {
-  name: string;
-  placeholder: string;
-  type: InputType;
-  value: string;
+export interface CampoListaItemProps extends CampoInputProps {
   name_messagem_erro: string;
 }
 
 export function CampoListaItem(props: CampoListaItemProps) {
-  const { name, placeholder, type, value, name_messagem_erro } = props;
+  const { id, name, placeholder, type, value, name_messagem_erro } = props;
 
   return (
     <div className="d-flex flex-column mb-1">
-      <Field
+      <CampoInput
+        id={id}
         name={name}
         placeholder={placeholder}
         type={type}
         value={value}
-        className="form-control"
       />
       <ErrorMessage
         name={name_messagem_erro}
