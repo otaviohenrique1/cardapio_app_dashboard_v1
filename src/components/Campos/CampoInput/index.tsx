4afc7d70723aea8a685758035b2e58a198ecdf38
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-05-03 21:01:37 -0300

    Reformulando arquivo

--- src/components/Campos/CampoInput/index.tsx
+++ src/components/Campos/CampoInput/index.tsx
@@ -1,14 +1,10 @@
 import { Field } from "formik";
-import { Col, Label, Alert } from "reactstrap";
+import { Col, Alert } from "reactstrap";
 import { ColumnProps } from "reactstrap/types/lib/Col";
+import { LabelCampoInput } from "../LabelCampoInput";
 
-export interface CampoInputProps {
+export interface CampoInputComErroProps extends CampoInputProps {
   label: string;
-  id: string;
-  name: string;
-  type: CampoInputTypes;
-  value: string;
-  placeholder: string;
   error?: any;
   touched?: any;
   md: ColumnProps;
@@ -19,17 +15,34 @@ export interface CampoInputProps {
   xxl?: ColumnProps;
 }
 
-export function CampoInput(props: CampoInputProps) {
+export function CampoInputComErro(props: CampoInputComErroProps) {
   const { id, label, name, type, value, placeholder, error, touched, md, xs, sm, lg, xl, xxl } = props;
 
   return (
     <Col md={md} xs={xs} sm={sm} lg={lg} xl={xl} xxl={xxl} className="d-flex flex-column mt-3">
-      <Label className="form-label" htmlFor={id}>{label}</Label>
-      <Field className="form-control" id={id} name={name}
+      <LabelCampoInput htmlFor={id} label={label} />
+      <CampoInput id={id} name={name}
         type={type} value={value} placeholder={placeholder} />
       {error && touched ? (
         <Alert color="danger">{error}</Alert>
       ) : null}
     </Col>
+  );
+}
+
+export interface CampoInputProps {
+  id: string;
+  name: string;
+  type: CampoInputTypes;
+  value: string;
+  placeholder: string;
+}
+
+export function CampoInput(props: CampoInputProps) {
+  const { id, name, type, value, placeholder } = props;
+
+  return (
+    <Field className="form-control" id={id} name={name}
+      type={type} value={value} placeholder={placeholder} />
   );
 }
